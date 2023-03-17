236978e2edb4d8b2f392b4576b6f85ca2aa557d3
MOFIJUL HAQUE <mofijulhoque995@gmail.com>
2023-03-17 15:09:46 +0530

    Image Searcher

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import "../styles/image.css"
+
 const Image = ({ data }) => {
     return (
         <>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4  ">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4  image__section">
 
-                {/* <div class="col"> */}
+                <div class="col">
                 <a href={data.urls.regular} target="_blank">
-                    <img src={data.urls.small} alt={data.alt_description} height="200px" className='object-fit-none border rounded'
+                    <img src={data.urls.small} alt={data.alt_description}
                      />
                 </a>
-                {/* </div> */}
+                </div>
             </div>
 
         </>
--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -4,45 +4,70 @@ import { ImageContext } from '../App';
 const SearchField = () => {
 
     const [searchvalue, setSearchValue] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [suggestionClicked, setSuggestionClicked] = useState(false); // new state variable
 
     // context
     const { fetchData, setSearchImage } = useContext(ImageContext)
 
+    const handleInputChange = async (e) => {
+        const query = e.target.value;
+        setSearchValue(query);
+        setSuggestionClicked(false); // reset suggestionClicked
 
-    const handleInputChange = (e) => {
-        setSearchValue(e.target.value);
+        if (query) {
+            const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=VOGHC84n13kR4u5a9iGRBIiDYIbB4Bd-iPrMPyQXvLk`);
+            const data = await response.json();
+            const suggestions = data.results.slice(0, 5).map(result => result.description || result.alt_description);
+            setSuggestions(suggestions);
+        } else {
+            setSuggestions([]);
+        }
     }
 
+    const handleSuggestionClick = (suggestion) => {
+        setSearchValue(suggestion);
+        handleButtonSearch();
+        setSuggestionClicked(true); // set suggestionClicked to true
+    }
 
     const handleButtonSearch = () => {
         fetchData(`search/photos?page=1&query=${searchvalue}&client_id=VOGHC84n13kR4u5a9iGRBIiDYIbB4Bd-iPrMPyQXvLk`)
         setSearchValue("");
         setSearchImage(searchvalue)
-
+        setSuggestionClicked(false); // reset suggestionClicked
     }
-
-
 
     const handleEnterSearch = (e) => {
         if (e.key === "Enter") {
-            fetchData(`search/photos?page=1&query=${searchvalue}&client_id=VOGHC84n13kR4u5a9iGRBIiDYIbB4Bd-iPrMPyQXvLk`)
-            setSearchValue("");
-            setSearchImage(searchvalue)
-
+            handleButtonSearch();
+            setSuggestionClicked(false); // reset suggestionClicked
         }
     }
+
     return (
-        <div className='container text-center pb-4'>
-            <input
-                type="search"
-                className='form-control'
-                placeholder='Search anything...'
-                value={searchvalue}
-                onChange={handleInputChange}
-                onKeyDown={handleEnterSearch}
-
-
-            />
+        <div className='container text-center pb-5'>
+            <div style={{ position: 'relative' }}>
+                <input
+                    type="search"
+                    className='form-control'
+                    placeholder='Search anything...'
+                    value={searchvalue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleEnterSearch}
+                />
+                {!suggestionClicked && suggestions.length > 0 && ( // only render the list if a suggestion hasn't been clicked
+                    <div style={{ position: 'absolute', top: '100%',cursor:"pointer", left: '0', right: '0', zIndex: '1' }}>
+                        <ul className="list-group">
+                            {suggestions.map(suggestion => (
+                                <li key={suggestion} className="list-group-item" onClick={() => handleSuggestionClick(suggestion)}>
+                                    {suggestion}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
             <button
                 disabled={!searchvalue}
                 onClick={handleButtonSearch}
@@ -52,3 +77,9 @@ const SearchField = () => {
 }
 
 export default SearchField;
+
+
+
+
+
+
